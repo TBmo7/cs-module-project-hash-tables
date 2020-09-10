379a6764e89cc3b24877ea34a3b42ddffe9cea00
Troy Moore <tbmoore85@gmail.com>
2020-09-09 22:08:30 -0400

    finished word count

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,6 +1,32 @@
+import re
 def word_count(s):
     # Your code here
+    dictionary = dict()
+    #words = re.split('\s+',s)
+    #x = s.lower()
+    #words = x.split()
+    x = re.sub("[^0-9a-zA-z']+", ' ', s).rstrip()
+    y = x.lower()
+    words = y.split(' ')
 
+    for i in words:
+        value = 0
+        
+        if i == '':
+            return dictionary
+        
+        if i in dictionary:
+            value = dictionary.get(i)
+            value += 1
+            update = {i:value}
+            dictionary.update(update)
+            
+        
+        else:
+            value = 1
+            update = {i:value}
+            dictionary.update(update)
+    return dictionary
 
 
 if __name__ == "__main__":
