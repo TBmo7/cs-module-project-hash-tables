afd48be6e60c692e0041217340eea1b8a607f8ba
Troy Moore <tbmoore85@gmail.com>
2020-09-09 20:52:00 -0400

    finished expensive sequence

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,10 +1,18 @@
 # Your code here
-
+expensivedict = dict()
 
 def expensive_seq(x, y, z):
     # Your code here
-
-
+    key = x,y,z
+    if expensivedict.get(key) is not None:
+        return expensivedict.get(key)
+    if x <= 0: 
+       value =  y + z
+    if x >  0: 
+        value = expensive_seq(x-1,y+1,z) +  expensive_seq(x-2,y+2,z*2) +  expensive_seq(x-3,y+3,z*3)
+        update = {key:value}
+        expensivedict.update(update)
+    return value
 
 if __name__ == "__main__":
     for i in range(10):
