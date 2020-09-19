d8487a17332873e89156e963b9a777c88dde6d06
Troy Moore <tbmoore85@gmail.com>
2020-09-18 21:31:27 -0400

    uploaded

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -3,3 +3,33 @@
 
 # Your code here
 
+"""
+split the words > go over the letters in each word > attribute the key to frequency
+use key to make cipher  > apply cipher
+"""
+
+import re
+with open("ciphertext.txt") as f:
+    words = f.read()
+
+#x = re.sub("[^0-9a-zA-z']+", ' ', words).rstrip()
+#y = x.lower()
+words2 = words.split(' ')
+letter_holder = {}
+frequency_table = ['E', 'T', 'A', 'O', 'H', 'N', 'R', 'I', 'S', 'D', 'L', 'W', 'U',
+'G', 'F', 'B', 'M', 'Y', 'C', 'P', 'K', 'V', 'Q', 'J', 'X', 'Z']
+key_dict = {}
+#key dict will be what links the input letter with the proper one
+"""
+loop through each word looking for letters > if the letter isn't found add it
+if it is found update the value
+"""
+def frequency_analyzer(words_here):
+    for element in words_here:
+        for letter in element:
+            if letter in letter_holder:
+                letter_holder[letter] += 1
+            else letter_holder[letter] = 0
+
+def key_maker(word_input):
+    sorted_elements = sorted(word_input.items(), key = lambda x : x[1], reverse = True)
--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,34 @@
 # Your code here
+import re
+with open("robin.txt") as f:
+    words = f.read()
 
+x = re.sub("[^0-9a-zA-z']+", ' ', words).rstrip()
+y = x.lower()
+words2 = y.split(' ')
+word_holder = {}
+
+"""
+after the regex > put in key value pair into dict
+word is the key > number of occurrances is value
+when getting out put get each item in the dict 
+"""
+for element in words2:
+    if element in word_holder:
+        #if the word is found in the holder, increment
+        word_holder[element] += 1
+    else:
+        word_holder[element] = 1
+
+sorted_elements = sorted(sorted(word_holder.items(), key = lambda x: x[0]), key = lambda x : x[1], reverse = True)
+#use nested sorteds,using both the count and alphabetized, but reversing only the count
+#print(sorted_elements)
+
+#sorted_elements = word_holder.items()
+for element in sorted_elements:
+    out_hash = element[1]
+    #print(f'\n {element[0]}  {'#' * out_hash}')
+    print(f'{element[0]}  ', '#'* out_hash) 
+
+
+ 
